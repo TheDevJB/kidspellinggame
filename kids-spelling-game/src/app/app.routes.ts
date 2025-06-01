d9bd2b0bd78786dd6c03ea0c0d2c255dfb545551
Joewan <jabwanjoe@gmail.com>
2025-06-01 17:27:01 -0400

    new home page

--- kids-spelling-game/src/app/app.routes.ts
+++ kids-spelling-game/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'math', component: HomePageComponent }, // Placeholder for Math
+  { path: 'spelling', component: HomePageComponent } // Placeholder for Spelling
+];
